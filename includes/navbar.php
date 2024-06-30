5d306e22ccfbd6fc1685593d51045dfef05a47d7
Jomszxc <202210892@fit.edu.ph>
2024-06-30 19:17:34 +0800

    connected to about page

--- includes/navbar.php
+++ includes/navbar.php
@@ -9,7 +9,7 @@
               <a class="nav-link" href="/index.php">Home <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
-              <a class="nav-link" href="#">About</a>
+              <a class="nav-link" href="about.php">About</a>
           </li>
           <li class="nav-item">
               <a class="nav-link" href="#">Contact</a>

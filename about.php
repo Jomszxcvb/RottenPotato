6fb820aaf06766c34d52c0f76fcb7fbabec10b8d
AMF112 <adrianmiguelflores512@gmail.com>
2024-07-03 01:42:41 +0800

    added about.php design

--- about.php
+++ about.php
@@ -13,31 +13,83 @@
 </head>
 <body class="about">
     <?php include 'includes/navbar.php'; ?>
-    <div class="bg-black text-white about-us d-flex justify-content-center">
-        <div style="width:78%;">
-            <h1>About Rotten Potato</h1>
+    <div class="container-fluid bg-black p-3 text-white">
+        <div class="container text-center mb-5">
+            <h1>About Us</h1>
+        </div>
+        <div class="container d-flex justify-content-around">
+            <div class="card">
+                <img src="assets/movie_thumbnails/Parasite (2019).jpeg" alt="Parasite">
+                <div class="container text-center p-2">
+                    <h6><b>Parasite</b></h6> 
+                </div>
+            </div>
+            <div class="card">
+                <img src="assets/movie_thumbnails/Toy Story 4 (2019).jpeg" alt="Toy Story 4">
+                <div class="container text-center p-2">
+                    <h6><b>Toy Story 4</b></h6> 
+                </div>
+            </div>
+            <div class="card">
+                <img src="assets/movie_thumbnails/The Lion King (2019).png" alt="The Lion King">
+                <div class="container text-center p-2">
+                    <h6><b>The Lion King</b></h6> 
+                </div>
+            </div>
         </div>
     </div>
-    <div class="container pb-5">
-        <div class="mt-2">
-            <p>Welcome to Rotten Potato, your go-to platform for movie ratings and reviews! Inspired by the renowned Rotten Tomatoes, 
-            we provide a unique and fun way to rate movies on a scale of 1 to 5 potatoes.</p>
-        </div>
-        <div class="mt-5">
-            <h1>Our Mission</h1>
-            <p>At Rotten Potato, our mission is to help movie enthusiasts make informed decisions about what to watch. We believe that every movie has its own flavor, 
-            and our potato rating system offers a quirky and memorable way to capture the essence of each film.</p>
-        </div>
-        <div class="mt-5">
-            <h1>How It Works</h1>
-            <p>Our rating system is simple and straightforward:</p>
-            <ul>
-                <li>1 Potato: Not worth your time.</li>
-                <li>2 Potatoes: Has some redeeming qualities but falls short overall.</li>
-                <li>3 Potatoes: A decent watch with its share of highs and lows.</li>
-                <li>4 Potatoes: A great movie that most will enjoy.</li>
-                <li>5 Potatoes: An absolute must-see, a cinematic masterpiece!</li>
-            </ul>
+    <div class="container pb-5 pt-3 bg-white">
+        <div class="text-center mx-auto" style="width:90%;">
+            <hr>
+            <div class="mt-5 pb-5">
+                <h1>Welcome to Rotten Potato</h1>
+                <p>A go-to platform for movie ratings and reviews! Inspired by the renowned Rotten Tomatoes, 
+                we provide a unique and fun way to rate movies on a scale of 1 to 5 potatoes.</p>
+            </div>
+            <hr>
+            <div class="mt-5 pb-5">
+                <h1>Our Mission</h1>
+                <p>At Rotten Potato, our mission is to help movie enthusiasts make informed decisions about what to watch. We believe that every movie has its own flavor, 
+                and our potato rating system offers a quirky and memorable way to capture the essence of each film.</p>
+            </div>
+            <hr>
+            <div class="mt-5 pb-5">
+                <h1>How It Works</h1>
+                <p class="mt-4"><b>Our rating system is simple and straightforward</b></p>
+                <div class="d-flex justify-content-between mt-4">
+                        <div class="container">
+                            <div> <?php
+                                echo '<span class="movie_potato active"><img src="assets/potato/potato.svg" alt="active potato"></span>'; ?> 
+                            </div>
+                            <div> 1. Potato: Not worth your time. </div>
+                        </div>
+                        <div class="container">
+                            <div> <?php for($i = 0; $i < 2; $i++)
+                                echo '<span class="movie_potato active"><img src="assets/potato/potato.svg" alt="active potato"></span>'; ?> 
+                            </div>
+                            <div>2. Potatoes: Has some redeeming qualities but falls short overall.</div>
+                        </div>
+                        <div class="container">
+                            <div> <?php for($i = 0; $i < 3; $i++)
+                                echo '<span class="movie_potato active"><img src="assets/potato/potato.svg" alt="active potato"></span>'; ?> 
+                            </div>
+                            <div>3. Potatoes: A decent watch with its share of highs and lows.</div>
+                        </div>
+                        <div class="container">
+                            <div> <?php for($i = 0; $i < 4; $i++)
+                                echo '<span class="movie_potato active"><img src="assets/potato/potato.svg" alt="active potato"></span>'; ?> 
+                            </div>
+                            <div>4. Potatoes: A great movie that most will enjoy.</div>
+                        </div>
+                        <div class="container">
+                            <div> <?php for($i = 0; $i < 5; $i++)
+                                echo '<span class="movie_potato active"><img src="assets/potato/potato.svg" alt="active potato"></span>'; ?> 
+                            </div>
+                            <div>5. Potatoes: An absolute must-see, a cinematic masterpiece!</div>
+                        </div>
+                </div>
+            </div>
+            <hr>
         </div>
     </div>
 

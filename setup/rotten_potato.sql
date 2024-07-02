c4c6c48a3b1b6bf0a9af24296389da5f40fbeb7e
Jomszxc <202210892@fit.edu.ph>
2024-07-02 15:59:30 +0800

    commit

--- setup/rotten_potato.sql
+++ setup/rotten_potato.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 02, 2024 at 08:33 AM
+-- Generation Time: Jul 02, 2024 at 09:59 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `movie` (
   `movie_id` int(11) NOT NULL,
   `thumbnail` varchar(255) NOT NULL,
-  `trailer_link` varchar(255) NOT NULL,
+  `trailer_id` varchar(255) NOT NULL,
   `title` varchar(100) NOT NULL,
   `synopsis` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -39,21 +39,21 @@ CREATE TABLE `movie` (
 -- Dumping data for table `movie`
 --
 
-INSERT INTO `movie` (`movie_id`, `thumbnail`, `trailer_link`, `title`, `synopsis`) VALUES
-(1, 'Avengers- Endgame (2019).jpg', 'https://www.youtube.com/watch?v=TcMBFSGVi1c', 'Avengers: Endgame', 'After the devastating events of Avengers: Infinity War, the universe is in ruins due to the efforts of the Mad Titan, Thanos. With the help of remaining allies, the Avengers must assemble once more in order to undo Thanos\' actions and restore order to the universe once and for all, no matter what consequences may be in store.'),
-(2, 'Joker (2019).png', 'https://www.youtube.com/watch?v=rgrWXTz_8eU', 'Joker', 'During the 1980s, a failed stand-up comedian is driven insane and turns to a life of crime and chaos in Gotham City while becoming an infamous psychopathic crime figure.'),
-(3, 'Spider-Man- Far From Home (2019).jpg', 'https://www.youtube.com/watch?v=Nt9L1jCKGnE', 'Spider-Man: Far From Home', 'Peter Parker and his friends go on a summer trip to Europe. However, they will hardly be able to rest - Peter will have to agree to help Nick Fury uncover the mystery of creatures that cause natural disasters and destruction throughout the continent.'),
-(4, 'Dark Phoenix (2019).jpg', 'https://www.youtube.com/watch?v=1-q8C_c-nlM', 'Dark Phoenix', 'The X-Men face their most formidable and powerful foe when one of their own, Jean Grey, starts to spiral out of control. During a rescue mission in outer space, Jean is nearly killed when she\'s hit by a mysterious cosmic force. Once she returns home, this force not only makes her infinitely more powerful, but far more unstable. The X-Men must now band together to save her soul and battle aliens that want to use Grey\'s new abilities to rule the galaxy.'),
-(5, 'Toy Story 4 (2019).jpeg', 'https://www.youtube.com/watch?v=wmiIUN-7qhE', 'Toy Story 4', 'Woody has always been confident about his place in the world and that his priority is taking care of his kid, whether that\'s Andy or Bonnie. But when Bonnie adds a reluctant new toy called \"Forky\" to her room, a road trip adventure alongside old and new friends will show Woody how big the world can be for a toy.'),
-(6, 'Shazam! (2019).jpg', 'https://www.youtube.com/watch?v=go6GEIrcvFY', 'Shazam!', 'A boy is given the ability to become an adult superhero in times of need with a single magic word.'),
-(7, 'Star Wars- The Rise of Skywalker (2019).jpeg', 'https://www.youtube.com/watch?v=8Qn_spdM5Zg', 'Star Wars: The Rise of Skywalker', 'The surviving Resistance faces the First Order once again as the journey of Rey, Finn and Poe Dameron continues. With the power and knowledge of generations behind them, the final battle begins.'),
-(8, 'Godzilla- King of the Monsters (2019).png', 'https://www.youtube.com/watch?v=QFxN2oDKk0E', 'Godzilla: King of the Monsters', 'Follows the heroic efforts of the crypto-zoological agency Monarch as its members face off against a battery of god-sized monsters, including the mighty Godzilla, who collides with Mothra, Rodan, and his ultimate nemesis, the three-headed King Ghidorah. When these ancient super-species, thought to be mere myths, rise again, they all vie for supremacy, leaving humanity\'s very existence hanging in the balance.'),
-(9, 'Parasite (2019).jpeg', 'https://www.youtube.com/watch?v=SEUXfv87Wpk', 'Parasite', 'All unemployed, Ki-taek\'s family takes peculiar interest in the wealthy and glamorous Parks for their livelihood until they get entangled in an unexpected incident.'),
-(10, 'Frozen II (2019).png', 'https://www.youtube.com/watch?v=Zi4LMpSDccc', 'Frozen II', 'Elsa, Anna, Kristoff and Olaf head far into the forest to learn the truth about an ancient mystery of their kingdom.'),
-(11, 'Alita- Battle Angel (2019).jpeg', 'https://www.youtube.com/watch?v=w7pYhpJaJW8', 'Alita: Battle Angel', 'When Alita awakens with no memory of who she is in a future world she does not recognize, she is taken in by Ido, a compassionate doctor who realizes that somewhere in this abandoned cyborg shell is the heart and soul of a young woman with an extraordinary past.'),
-(12, 'John Wick- Chapter 3 - Parabellum (2019).jpeg', 'https://www.youtube.com/watch?v=M7XM597XO94', 'John Wick: Chapter 3 - Parabellum', 'Super-assassin John Wick returns with a $14 million price tag on his head and an army of bounty-hunting killers on his trail. After killing a member of the shadowy international assassin’s guild, the High Table, John Wick is excommunicado, but the world’s most ruthless hit men and women await his every turn.'),
-(13, 'Terminator- Dark Fate (2019).png', 'https://www.youtube.com/watch?v=oxy8udgWRmo', 'Terminator: Dark Fate', 'Decades after Sarah Connor prevented Judgment Day, a lethal new Terminator is sent to eliminate the future leader of the resistance. In a fight to save mankind, battle-hardened Sarah Connor teams up with an unexpected ally and an enhanced super soldier to stop the deadliest Terminator yet.'),
-(14, 'It Chapter Two (2019).jpeg', 'https://www.youtube.com/watch?v=xhJ5P7Up3jA', 'It: Chapter Two', '27 years after overcoming the malevolent supernatural entity Pennywise, the former members of the Losers\' Club, who have grown up and moved away from Derry, are brought back together by a devastating phone call.');
+INSERT INTO `movie` (`movie_id`, `thumbnail`, `trailer_id`, `title`, `synopsis`) VALUES
+(1, 'Avengers- Endgame (2019).jpg', 'TcMBFSGVi1c', 'Avengers: Endgame', 'After the devastating events of Avengers: Infinity War, the universe is in ruins due to the efforts of the Mad Titan, Thanos. With the help of remaining allies, the Avengers must assemble once more in order to undo Thanos\' actions and restore order to the universe once and for all, no matter what consequences may be in store.'),
+(2, 'Joker (2019).png', 'rgrWXTz_8eU', 'Joker', 'During the 1980s, a failed stand-up comedian is driven insane and turns to a life of crime and chaos in Gotham City while becoming an infamous psychopathic crime figure.'),
+(3, 'Spider-Man- Far From Home (2019).jpg', 'Nt9L1jCKGnE', 'Spider-Man: Far From Home', 'Peter Parker and his friends go on a summer trip to Europe. However, they will hardly be able to rest - Peter will have to agree to help Nick Fury uncover the mystery of creatures that cause natural disasters and destruction throughout the continent.'),
+(4, 'Dark Phoenix (2019).jpg', '1-q8C_c-nlM', 'Dark Phoenix', 'The X-Men face their most formidable and powerful foe when one of their own, Jean Grey, starts to spiral out of control. During a rescue mission in outer space, Jean is nearly killed when she\'s hit by a mysterious cosmic force. Once she returns home, this force not only makes her infinitely more powerful, but far more unstable. The X-Men must now band together to save her soul and battle aliens that want to use Grey\'s new abilities to rule the galaxy.'),
+(5, 'Toy Story 4 (2019).jpeg', 'wmiIUN-7qhE', 'Toy Story 4', 'Woody has always been confident about his place in the world and that his priority is taking care of his kid, whether that\'s Andy or Bonnie. But when Bonnie adds a reluctant new toy called \"Forky\" to her room, a road trip adventure alongside old and new friends will show Woody how big the world can be for a toy.'),
+(6, 'Shazam! (2019).jpg', 'go6GEIrcvFY', 'Shazam!', 'A boy is given the ability to become an adult superhero in times of need with a single magic word.'),
+(7, 'Star Wars- The Rise of Skywalker (2019).jpeg', '8Qn_spdM5Zg', 'Star Wars: The Rise of Skywalker', 'The surviving Resistance faces the First Order once again as the journey of Rey, Finn and Poe Dameron continues. With the power and knowledge of generations behind them, the final battle begins.'),
+(8, 'Godzilla- King of the Monsters (2019).png', 'QFxN2oDKk0E', 'Godzilla: King of the Monsters', 'Follows the heroic efforts of the crypto-zoological agency Monarch as its members face off against a battery of god-sized monsters, including the mighty Godzilla, who collides with Mothra, Rodan, and his ultimate nemesis, the three-headed King Ghidorah. When these ancient super-species, thought to be mere myths, rise again, they all vie for supremacy, leaving humanity\'s very existence hanging in the balance.'),
+(9, 'Parasite (2019).jpeg', 'SEUXfv87Wpk', 'Parasite', 'All unemployed, Ki-taek\'s family takes peculiar interest in the wealthy and glamorous Parks for their livelihood until they get entangled in an unexpected incident.'),
+(10, 'Frozen II (2019).png', 'Zi4LMpSDccc', 'Frozen II', 'Elsa, Anna, Kristoff and Olaf head far into the forest to learn the truth about an ancient mystery of their kingdom.'),
+(11, 'Alita- Battle Angel (2019).jpeg', 'w7pYhpJaJW8', 'Alita: Battle Angel', 'When Alita awakens with no memory of who she is in a future world she does not recognize, she is taken in by Ido, a compassionate doctor who realizes that somewhere in this abandoned cyborg shell is the heart and soul of a young woman with an extraordinary past.'),
+(12, 'John Wick- Chapter 3 - Parabellum (2019).jpeg', 'M7XM597XO94', 'John Wick: Chapter 3 - Parabellum', 'Super-assassin John Wick returns with a $14 million price tag on his head and an army of bounty-hunting killers on his trail. After killing a member of the shadowy international assassin’s guild, the High Table, John Wick is excommunicado, but the world’s most ruthless hit men and women await his every turn.'),
+(13, 'Terminator- Dark Fate (2019).png', 'oxy8udgWRmo', 'Terminator: Dark Fate', 'Decades after Sarah Connor prevented Judgment Day, a lethal new Terminator is sent to eliminate the future leader of the resistance. In a fight to save mankind, battle-hardened Sarah Connor teams up with an unexpected ally and an enhanced super soldier to stop the deadliest Terminator yet.'),
+(14, 'It Chapter Two (2019).jpeg', 'xhJ5P7Up3jA', 'It: Chapter Two', '27 years after overcoming the malevolent supernatural entity Pennywise, the former members of the Losers\' Club, who have grown up and moved away from Derry, are brought back together by a devastating phone call.');
 
 -- --------------------------------------------------------
 

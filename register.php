0f789222974bfd1e98bff7d024f70f462d259e61
AMF112 <adrianmiguelflores512@gmail.com>
2024-07-05 18:13:05 +0800

    commit

--- register.php
+++ register.php
@@ -114,7 +114,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <hr class="opacity-75 mt-4" />
             <p>Already have an account? <a href="login.php">Login here</a>.</p>
         </form>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
